4a211c56ddd160ca535594cd8a7b53def05dbf8f
mishael-codes <enyimishael22@gmail.com>
2023-11-01 10:49:38 +0100

    added custom color and css reset values

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,12 @@ export default {
   ],
   theme: {
     extend: {},
+    colors: {
+      background: "hsl(218, 20%, 16%)",
+      textColor: "hsl(0, 0%, 93%)",
+      accentColor: "hsl(42, 100%, 71%)",
+      shadowColor: "hsl(217, 10%, 25%)",
+    },
   },
   plugins: [],
 }

b2edb143d192f106ef332faee02d9ba157c8a09e
mishael-codes <enyimishael22@gmail.com>
2023-11-02 23:20:15 +0100

    routing and began work on the navbar

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Nav from "./components/nav.jsx";
+import About from "./components/pages/about";
+import Contact from "./components/pages/contact";
+import Home from "./components/pages/home";
+import Projects from "./components/pages/projects";
+import Skills from "./components/pages/skills";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/skills" element={<Skills />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact/>}/>
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;

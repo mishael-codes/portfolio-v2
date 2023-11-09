62806cc941ae9f58e3235069eb000c27242a6768
mishael-codes <enyimishael22@gmail.com>
2023-11-09 16:03:58 +0100

    Worked on the home page
     - added react-type-animation
     - used react-type-animation to animate the text on the home page

--- src/components/pages/home.jsx
+++ src/components/pages/home.jsx
@@ -1,27 +1,48 @@
-import { useState } from "react";
+import { TypeAnimation } from "react-type-animation";
 import backgroundImg from "../../assets/images/bg/background-img.jpg";
 import heroImageMobile from "../../assets/images/hero_img.jpeg";
 const Home = () => {
-  
-
   return (
     <>
       <div className="relative">
         <img
           src={backgroundImg}
           alt="background.jpg"
-          className="absolute opacity-90 w-full h-screen"
+          className="absolute opacity-70 w-full h-screen"
         />
         <div className="flex items-start justify-center h-screen">
-          <h3 className="absolute w-full text-center text-3xl font-bold text-accentColor">
-            Hello
-          </h3>
           <img
             src={heroImageMobile}
             alt=""
-            className="absolute top-20 w-1/2 rounded-full"
+            className="absolute top-20 w-1/2 rounded-full opacity-80"
           />
-          <h2 className="absolute top-72">My name is <strong>Mishael Enyi</strong></h2>
+          <div className="absolute top-72 text-center">
+            <h2 className="text-textColor">
+              My name is <br />
+              <strong className="text-accentColor">Mishael Enyi</strong>
+            </h2>
+            <br />
+            <p className="text-textColor">
+              I am a <br />
+              <TypeAnimation
+                preRenderFirstString={true}
+                sequence={[
+                  "Frontend Developer",
+                  1000,
+                  "React Developer",
+                  1000,
+                  "Vue Developer",
+                  1000,
+                ]}
+                wrapper="strong"
+                speed={20}
+                deletionSpeed={40}
+                repeat={Infinity}
+                cursor={true}
+                className="text-accentColor"
+              />
+            </p>
+          </div>
         </div>
       </div>
     </>

2e856564ae7af3975a3dfb3de91c4f94c0982b1f
mishael-codes <enyimishael22@gmail.com>
2023-11-09 16:22:16 +0100

    homepage responsiveness

--- src/components/pages/home.jsx
+++ src/components/pages/home.jsx
@@ -4,24 +4,23 @@ import heroImageMobile from "../../assets/images/hero_img.jpeg";
 const Home = () => {
   return (
     <>
-      <div className="relative">
+      <div className="relative bg-background">
         <img
           src={backgroundImg}
           alt="background.jpg"
-          className="absolute opacity-70 w-full h-screen"
+          className="absolute opacity-80 w-full h-screen"
         />
-        <div className="flex items-start justify-center h-screen">
+        <div className="absolute flex items-center justify-center flex-col pb-28 px-5 h-screen md:flex-row-reverse md:justify-between">
           <img
             src={heroImageMobile}
             alt=""
-            className="absolute top-20 w-1/2 rounded-full opacity-80"
+            className=" w-1/2 rounded-full opacity-90"
           />
-          <div className="absolute top-72 text-center">
-            <h2 className="text-textColor">
+          <div className="w-full md:w-1/2 text-center mt-4 md:text-3xl">
+            <h2 className="text-textColor opacity-100">
               My name is <br />
               <strong className="text-accentColor">Mishael Enyi</strong>
             </h2>
-            <br />
             <p className="text-textColor">
               I am a <br />
               <TypeAnimation

ef7b1381920d1a2bf698d4508138823573190afc
mishael-codes <enyimishael22@gmail.com>
2023-11-12 22:41:06 +0100

    added contact page   - updated the metadata for index.html   - added fontawesome icons library and component   - minor changes to the about page

--- src/components/pages/about.jsx
+++ src/components/pages/about.jsx
@@ -3,38 +3,44 @@ const About = () => {
   return (
     <>
       <main className="w-screen relative">
-        <img src={backgroundImg} alt="" className="absolute z-0 h-screen w-screen opacity-50" />
-{        <div className="absolute h-screen overflow-auto">
-          <div className="w-full text-center">
-            <h1 className="text-textColor text-xl font-semibold uppercase underline underline-offset-2 decoration-accentColor mt-3 md:text-4xl">
-              About
-            </h1>
+        <img
+          src={backgroundImg}
+          alt=""
+          className="absolute z-0 h-screen w-screen opacity-50"
+        />
+        {
+          <div className="absolute h-screen overflow-auto">
+            <div className="w-full text-center">
+              <h1 className="text-textColor text-xl font-semibold uppercase underline underline-offset-2 decoration-accentColor mt-3 md:text-4xl">
+                About
+              </h1>
+            </div>
+            <div className="w-full relative p-6 text-textColor md:text-2xl md:pl-24 lg:flex lg:flex-col lg:items-start lg:justify-center lg:mt-24 xl:mt-0 lg:h-[700px] lg:w-1/2">
+              <p className="first-letter:text-2xl md:first-letter:text-5xl first-letter:text-accentColor first-letter:font-semibold indent-3 mb-2">
+                Hello, it's nice to see that you're here👋. Here's a little
+                knowledge about me.
+              </p>
+              <p className="first-letter:text-2xl md:first-letter:text-5xl first-letter:text-accentColor first-letter:font-semibold indent-3 mb-2">
+                I wrote my first line of code in 2021, It was in HTML and ever
+                since then, I have always wanted to learn more to see what I can
+                build and how I can be of importance in creating websites which
+                offer information, entertainment, education and so on to the
+                public.
+              </p>
+              <p className="first-letter:text-2xl md:first-letter:text-5xl first-letter:text-accentColor first-letter:font-semibold indent-3 mb-2">
+                As a side note, I have a little bit of interest in game
+                development and hope to venture into it some time in the future.
+                I also have an interest in photography, nature photography to be
+                precise. I read webtoons in my spare time and I listen to music
+                with every chance I get.
+              </p>
+              <p className="first-letter:text-2xl md:first-letter:text-5xl first-letter:text-accentColor first-letter:font-semibold indent-3">
+                I hope that was enough for a basic introduction. Let's work
+                together to fulfill both your dreams and mine, shall we?
+              </p>
+            </div>
           </div>
-          <div className="w-full relative p-6 text-textColor md:text-2xl md:pl-24 lg:flex lg:flex-col lg:items-start lg:justify-center lg:mt-24 xl:mt-0 lg:h-[700px] lg:w-1/2">
-            <p className="first-letter:text-2xl md:first-letter:text-5xl first-letter:text-accentColor first-letter:font-semibold indent-3 mb-2">
-              Hello, it's nice to see that you're here👋. Here's a little
-              knowledge about me.
-            </p>
-            <p className="first-letter:text-2xl md:first-letter:text-5xl first-letter:text-accentColor first-letter:font-semibold indent-3 mb-2">
-              I wrote my first line of code in 2021, It was in HTML and ever
-              since then, I have always wanted to learn more to see what I can
-              build and how I can be of importance in creating websites which
-              offer information, entertainment, education and so on to the
-              public.
-            </p>
-            <p className="first-letter:text-2xl md:first-letter:text-5xl first-letter:text-accentColor first-letter:font-semibold indent-3 mb-2">
-              As a side note, I have a little bit of interest in game
-              development and hope to venture into it some time in the future. I
-              also have an interest in photography, nature photography to be
-              precise. I read webtoons in my spare time and I listen to music
-              with every chance I get.
-            </p>
-            <p className="first-letter:text-2xl md:first-letter:text-5xl first-letter:text-accentColor first-letter:font-semibold indent-3">
-              I hope that was enough for a basic introduction. Let's work
-              together to fulfill both your dreams and mine, shall we?
-            </p>
-          </div>
-        </div>}
+        }
       </main>
     </>
   );
--- src/components/pages/contact.jsx
+++ src/components/pages/contact.jsx
@@ -1,9 +1,83 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faXTwitter,
+  faLinkedin,
+  faGithub,
+  faInstagram,
+} from "@fortawesome/free-brands-svg-icons";
+
 const Contact = () => {
   return (
     <>
-      <h1>Contact</h1>
+      <div className="h-screen w-screen flex flex-col items-center">
+        <h1 className="mt-4 mb-8 text-textColor text-xl font-semibold uppercase underline underline-offset-2 decoration-accentColor">
+          Want to work together?
+        </h1>
+
+        <form
+          action=""
+          method="post"
+          className="flex flex-col items-center justify-start w-3/4"
+        >
+          <input
+            type="text"
+            name="Full Name"
+            placeholder="John Doe"
+            className="w-full mb-6 bg-shadowColor p-2 rounded-lg"
+          />
+          <input
+            type="email"
+            name="email"
+            placeholder="johndoe@gmail.com"
+            className="w-full mb-6 bg-shadowColor p-2 rounded-lg"
+          />
+          <textarea
+            name="message"
+            rows="7"
+            placeholder="Type your message"
+            className="w-full mb-6 bg-shadowColor p-2 rounded-lg"
+            required
+          ></textarea>
+          <button
+            type="submit"
+            className="w-full bg-accentColor text-background font-semibold p-2 rounded-lg"
+          >
+            SUBMIT
+          </button>
+        </form>
+        <div className="w-full flex items-center justify-evenly mt-8">
+          <a target="" href="https://github.com/mishael-codes">
+            <FontAwesomeIcon
+              icon={faGithub}
+              size="2xl"
+              className="text-textColor hover:text-accentColor"
+            />
+          </a>
+          <a target="" href="https://www.linkedin.com/in/mishael-enyi/">
+            <FontAwesomeIcon
+              icon={faLinkedin}
+              size="2xl"
+              className="text-textColor hover:text-accentColor"
+            />
+          </a>
+          <a target="" href="https://twitter.com/mishael_codes">
+            <FontAwesomeIcon
+              icon={faXTwitter}
+              size="2xl"
+              className="text-textColor hover:text-accentColor"
+            />
+          </a>
+          <a target="" href="https://www.instagram.com/mishael_codes/">
+            <FontAwesomeIcon
+              icon={faInstagram}
+              size="2xl"
+              className="text-textColor hover:text-accentColor"
+            />
+          </a>
+        </div>
+      </div>
     </>
   );
 };
 
-export default Contact
+export default Contact;

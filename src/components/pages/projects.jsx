c86bc0d3327dc98547a910a4fc4e77a0531250c7
mishael-codes <enyimishael22@gmail.com>
2023-11-15 23:55:12 +0100

    unfinished   - started working on the projects page   - made an api call to my github repos and display the items on the page   - added ome unfinished styles

--- src/components/pages/projects.jsx
+++ src/components/pages/projects.jsx
@@ -1,9 +1,45 @@
+import { useState, useEffect } from "react";
+// import { Link } from "react-router-dom";
+
 const Projects = () => {
+  const [repos, setRepos] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const baseUrl = "https://api.github.com/users/mishael-codes";
+
+        const endPoint = `${baseUrl}/repos`;
+        const response = await fetch(endPoint);
+
+        if (response.status !== 200) {
+          console.log(`Error: ${response.status}`);
+        } else {
+          const data = await response.json();
+          console.log(data);
+          setRepos(data);
+        }
+      } catch (error) {
+        console.log("error");
+      }
+    })();
+  }, []);
+
   return (
     <>
-      <h1>Projects</h1>
+      <div className="text-textColor h-auto">
+        <h1>Projects</h1>
+        <div className="w-screen grid grid-cols-2 gap-2 place-content-center">
+          {repos.map((repo) => (
+            <div key={repo.id} className="w-full border rounded-xl p-4">
+              <h3 className="text-accentColor">{repo.name}</h3>
+              <p>{repo.description}</p>
+            </div>
+          ))}
+        </div>
+      </div>
     </>
   );
 };
 
-export default Projects
+export default Projects;

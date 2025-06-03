a70e63b7b1e31c7c996c1b7eee9f9483f4aa520e
mishael-codes <enyimishael22@gmail.com>
2025-06-03 11:11:21 +0100

    Fix: fix image of tailwindcss icon

--- vite.config.js
+++ vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
         short_name: "Mishael",
         start_url: "/",
         display: "standalone",
-        background_color: "#ffffff",
+        background_color: "#000000",
         theme_color: "#000000",
         icons: [
           {

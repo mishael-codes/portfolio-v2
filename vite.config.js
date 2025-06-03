182a3eecf73fd25f6c1226bf0cc6f57e872d7c62
mishael-codes <enyimishael22@gmail.com>
2025-06-03 09:19:56 +0100

    Feat: add PWA

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,33 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import {VitePWA} from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Mishael Enyi",
+        short_name: "Mishael",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#ffffff",
+        theme_color: "#000000",
+        icons: [
+          {
+            src: "icon_192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "icon_512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+});

8d784b99038ed864783e9666bfe9052d0bd6bf3c
Mishael Enyi <enyimishael22@gmail.com>
2026-03-13 09:40:18 +0100

    Chore: remove pwa as it is not compatible with current vite version

--- vite.config.js
+++ vite.config.js
@@ -1,48 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react(),
-    VitePWA({
-      registerType: "autoUpdate",
-      manifest: {
-        name: "Mishael Enyi",
-        short_name: "Mishael",
-        start_url: "/",
-        display: "standalone",
-        background_color: "#000000",
-        theme_color: "#000000",
-        icons: [
-          {
-            src: "./icon_192.png",
-            sizes: "192x192",
-            type: "image/png",
-          },
-          {
-            src: "./icon_512.png",
-            sizes: "512x512",
-            type: "image/png",
-          },
-        ],
-      },
-      workbox: {
-        runtimeCaching: [
-          {
-            urlPattern: ({ request }) => request.destination === "image",
-            handler: "CacheFirst",
-            options: {
-              cacheName: "images-cache",
-              expiration: {
-                maxEntries: 50,
-                maxAgeSeconds: 60 * 60 * 24 * 30, // 30 days
-              },
-            },
-          },
-        ],
-      },
-    }),
+    react()
   ],
 });
